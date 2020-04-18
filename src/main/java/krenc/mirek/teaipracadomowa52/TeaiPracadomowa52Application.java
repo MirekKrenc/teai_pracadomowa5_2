624645c77b4f269ee6e32a8802bb627ea965d6fa
kremir <kremir@o2.pl>
2020-04-19 00:39:39 +0200

    Some ammendments

---
+++ src/main/java/krenc/mirek/teaipracadomowa52/TeaiPracadomowa52Application.java
@@ -0,0 +1,13 @@
+package krenc.mirek.teaipracadomowa52;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaiPracadomowa52Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaiPracadomowa52Application.class, args);
+    }
+
+}
---
+++ src/test/java/krenc/mirek/teaipracadomowa52/TeaiPracadomowa52ApplicationTests.java
@@ -0,0 +1,13 @@
+package krenc.mirek.teaipracadomowa52;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaiPracadomowa52ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
